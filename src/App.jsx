ccff5f5687d37e6cfdb32a30c558c0a46ad6a8bd
zen <zenon.deviagge@gmail.com>
2024-03-12 20:52:46 -0300

    Render items from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
+import menu from './data'
 import Title from './Title'
+import Menu from './Menu'
 
 const App = () => {
+const [menuItems, setMenuItems] =  useState(menu);
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
