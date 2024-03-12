e00bed94dee95535e08fb39f6a858641d333584c
zen <zenon.deviagge@gmail.com>
2024-03-12 20:25:13 -0300

    Add Menu Title component.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Title from './Title'
+
+const App = () => {
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+      </section>
+    </main>
+  )
+}
+export default App
