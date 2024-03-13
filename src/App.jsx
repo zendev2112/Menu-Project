b6886c1830ca0e192aa14ad09e33ce277cf62e6f
zen <zenon.deviagge@gmail.com>
2024-03-12 21:12:20 -0300

    Add items unique values using new Set()

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import menu from './data'
 import Title from './Title'
 import Menu from './Menu'
 
+const tempCategories = menu.map((item)=>item.category)
+const tempSet = new Set(tempCategories)
+const tempItems = ['all',...tempSet]
+console.log(tempItems);
+
+const allCategories = ['all', ...new Set(menu.map((item)=>item.category))]
+
 const App = () => {
 const [menuItems, setMenuItems] =  useState(menu);
+const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
