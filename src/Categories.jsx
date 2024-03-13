29d4b73d4bfee7d4c306159b8020a26d2f96e931
zen <zenon.deviagge@gmail.com>
2024-03-12 21:36:09 -0300

    Filter items by category.

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,8 +1,8 @@
-const Categories = ({categories}) => {
+const Categories = ({categories, filterItems}) => {
   return (
     <div className="btn-container">
         {categories.map((category)=>{
-            return <button type="button" className="btn">
+            return <button type="button" className="btn" key={category} onClick={() => filterItems(category)}>
                 {category}
             </button>
         })}
